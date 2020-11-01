3f3931b0bce806154042442541efae20a636e160
Niall Phelan <niall@medtec.ie>
2020-11-01 18:56:42 +0000

    basic app working with hillfort list and edit functionality

---
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/HillfortListActivity.kt
@@ -0,0 +1,61 @@
+package org.wit.assignment1_hillfort.activities
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivityForResult
+import org.wit.assignment1_hillfort.R
+import org.wit.assignment1_hillfort.main.MainApp
+import org.wit.assignment1_hillfort.models.HillfortModel
+
+class HillfortListActivity : AppCompatActivity(), HillfortListener {
+
+    lateinit var app: MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort_list)
+        app = application as MainApp
+
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        loadHillforts()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.item_add -> startActivityForResult<HillfortActivity>(0)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onHillfortClick(hillfort: HillfortModel) {
+        startActivityForResult(intentFor<HillfortActivity>().putExtra("hillfort_edit", hillfort), 0)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        loadHillforts()
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun loadHillforts() {
+        showHillforts(app.hillforts.findAll())
+    }
+
+    fun showHillforts (hillforts: List<HillfortModel>) {
+        recyclerView.adapter = HillfortAdapter(hillforts, this)
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/org/wit/assignment1_hillfort/models/HillfortStore.kt
@@ -0,0 +1,9 @@
+package org.wit.assignment1_hillfort.models
+
+interface HillfortStore {
+    fun findAll(): List<HillfortModel>
+    fun create(hillfort: HillfortModel)
+    fun update(hillfort: HillfortModel)
+}
+
+
