eb90179c485e547cf643817cf3b8c430ac1af57a
Niall Phelan <niall@medtec.ie>
2020-11-01 20:59:56 +0000

    Simple splash screen with theme and noHistory added to the application

---
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/SplashActivity.kt
@@ -0,0 +1,27 @@
+package org.wit.assignment1_hillfort.activities
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+
+import androidx.appcompat.app.AppCompatActivity
+import org.wit.assignment1_hillfort.main.MainApp
+
+
+class SplashActivity : AppCompatActivity() {
+
+   private val SPLASH_TIME_OUT:Long=3000 // 3 sec
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super .onCreate(savedInstanceState)
+       Handler().postDelayed({
+       startActivity(Intent(this,HillfortListActivity::class.java))
+           finish()
+       }, SPLASH_TIME_OUT)
+   }
+}
+
+
+
+
