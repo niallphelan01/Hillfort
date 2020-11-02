e2b3bc579be64c1694c359bf27256f882e42c0e5
Niall Phelan <niall@medtec.ie>
2020-11-02 19:34:15 +0000

    user email password and userlevel added to model

--- app/src/main/java/org/wit/assignment1_hillfort/models/HillfortModel.kt
+++ app/src/main/java/org/wit/assignment1_hillfort/models/HillfortModel.kt
@@ -10,10 +10,19 @@ data class HillfortModel(var id: Long = 0,
                           var image: String = "",
                           var lat : Double = 0.0,
                           var lng: Double = 0.0,
-                          var zoom: Float = 0f) : Parcelable
+                          var zoom: Float = 0f,
+                          var email: String = "",
+                          var password: String = "",
+                          var userlevel: String = "") : Parcelable //user level to indicate basic or admin roles
 
 
 @Parcelize
 data class Location(var lat: Double = 0.0,
                     var lng: Double = 0.0,
                     var zoom: Float = 0f) : Parcelable
+
+
+@Parcelize
+data class User (var email: String = "",
+                 var password: String = "",
+                 var userlevel: String = ""): Parcelable //user level to indicate basic or admin roles
