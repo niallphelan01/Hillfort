5bef33fa6970866256225e3a28f7dbd1e280ff58
Niall Phelan <niall@medtec.ie>
2020-11-02 20:06:28 +0000

    added reset button to the user login page

--- app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 import org.wit.assignment1_hillfort.R
 
 class LoginActivity : AppCompatActivity() {
@@ -13,5 +14,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //var password = findViewById(R.id.password) as EditText
+
+    btnReset.setOnClickListener{
+        password.setText("")
+        user_name.setText("")
     }
+    }
+
+
 }
