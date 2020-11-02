979460370d6f1c7f8fe1dc093c4a87b985ca90ef
Niall Phelan <niall@medtec.ie>
2020-11-02 19:55:47 +0000

    added activity and layout for the login page

---
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
@@ -0,0 +1,17 @@
+package org.wit.assignment1_hillfort.activities
+
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import org.wit.assignment1_hillfort.R
+
+class LoginActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
