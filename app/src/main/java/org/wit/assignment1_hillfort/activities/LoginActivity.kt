1a18d641f5380dea8775ba181edc11f84989308f
Niall Phelan <niall@medtec.ie>
2020-11-02 20:22:37 +0000

    added some logic for the submit button for empty strings

--- app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
@@ -1,10 +1,12 @@
 package org.wit.assignment1_hillfort.activities
 
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.toast
 import org.wit.assignment1_hillfort.R
 
 class LoginActivity : AppCompatActivity() {
@@ -15,13 +17,29 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //var password = findViewById(R.id.password) as EditText
+    btnSubmit.setOnClickListener{
+        val user_name:String? = user_name.text.toString()
+        val password = password.text.toString()
+        if (isEmpty(user_name)) {
+            toast("Enter username")
+        }
+           else if (isEmpty(password)) {
+            toast("Enter password please ")
+        }
+          else{
+                //now check for username and password in the memory and open hillfort activity
 
-    btnReset.setOnClickListener{
+            }
+        }
+
+
+    btnReset.setOnClickListener{ //Rest values entered into the screen in case of errors
         password.setText("")
         user_name.setText("")
     }
     }
+
+
 
 
 }
