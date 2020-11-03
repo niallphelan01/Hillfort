c7180c1a962d36a2c8684aa18e761d90ef030e61
Niall Phelan <niall@medtec.ie>
2020-11-03 20:31:17 +0000

    basic signup added to the application and user check now working with basic password check also

--- app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/LoginActivity.kt
@@ -1,21 +1,28 @@
 package org.wit.assignment1_hillfort.activities
 
+import org.wit.assignment1_hillfort.activities.HillfortListActivity
+
+
+
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils.isEmpty
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.toast
 import org.wit.assignment1_hillfort.R
 import org.wit.assignment1_hillfort.main.MainApp
 import org.wit.assignment1_hillfort.models.UsersModel
-
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
     private var user = UsersModel()
     lateinit var app: MainApp
-    var loggedinUser = UsersModel()
+    var loggedinUser: UsersModel? = UsersModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,13 +44,21 @@ class LoginActivity : AppCompatActivity() {
             user.email = user_name
           }
           user.password = password
-          loggedinUser = app.users.findUsers(user.copy())!!
+          loggedinUser = this.app.users.findUsers(user.copy())
 
 
-            //add logic to allow for null or then the specific user
-            startActivity(Intent(this,HillfortListActivity::class.java)) //start the hillfort activity if username and password match
-            finish()
-            }
+          if (loggedinUser?.email  == user_name){
+           //add logic to allow for null or then the specific user
+            startActivity(Intent(this, HillfortListActivity::class.java)) //start the hillfort activity if username and password match
+            finish()}
+          else{
+            toast("username or email not found")
+           }
+          }
+
+
+
+
         }
 
 
@@ -53,10 +68,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
         btnSignup.setOnClickListener{
-            user.email = "test3"
-            user.password = "test3"
-            user.userlevel="basic3"
+          if (isEmpty(user_name.text.toString())) {
+            toast("Enter username")
+          }
+          else if (isEmpty(password.text.toString())) {
+            toast("Enter password please ")
+          }
+
+         // TODO("Maybe add feature to check for email type...@test.com input validation")
+             user.email= user_name.text.toString()
+             user.password = password.text.toString()
+            user.userlevel="basic"
             app.users.createUsers(user.copy())
+
+          toast("the user has been created now enter and submit to proceed")
 
 
         }
--- app/src/main/java/org/wit/assignment1_hillfort/activities/SplashActivity.kt
+++ app/src/main/java/org/wit/assignment1_hillfort/activities/SplashActivity.kt
@@ -17,7 +17,7 @@ class SplashActivity : AppCompatActivity() {
         super .onCreate(savedInstanceState)
        Handler().postDelayed({
       // startActivity(Intent(this,HillfortListActivity::class.java))
-         startActivity(Intent(this,LoginActivity::class.java))
+         startActivity(Intent(this, LoginActivity::class.java))
            finish()
        }, SPLASH_TIME_OUT)
    }
