0ab8039816e0081a5cad5e096def74678e8a1fae
Niall Phelan <niall@medtec.ie>
2020-11-08 20:51:28 +0000

    visited check box added

--- app/src/main/java/org/wit/assignment1_hillfort/models/HillfortModel.kt
+++ app/src/main/java/org/wit/assignment1_hillfort/models/HillfortModel.kt
@@ -8,6 +8,7 @@ data class HillfortModel(
     var id: Long = 0,
     var name: String = "",
     var description: String = "",
+    var visited: Boolean = false,
     var image: String="",
     var lat: Double = 0.0,
     var lng: Double = 0.0,
